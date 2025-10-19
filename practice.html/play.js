d9e57564111dc3e7af5b586f0f195f4663704f9a
David Omelianowicz <david_omeli@hotmail.com>
2025-10-19 15:24:48 +0100

    quote genatator

--- practice.html/play.js
+++ practice.html/play.js
@@ -2,7 +2,7 @@ let stone = document.getElementById("stone");
 let moodRing = document.getElementById("moodRing");
 
 stone.onclick = function changeColor() {
-  randomNumber = Math.floor(Math.random() * 8) + 1;
+  let randomNumber = Math.floor(Math.random() * 8) + 1;
 
   if (randomNumber === 1) {
     stone.style.backgroundColor = "red";
@@ -22,3 +22,33 @@ stone.onclick = function changeColor() {
     stone.style.backgroundColor = "black";
   }
 };
+
+const quoteList = [
+  "Some doors only open from the inside. Breath is a way of accessing that door.",
+  "What has to be taught first, is the breath.",
+  "Remember the blue sky. It may at times be obscured by clouds, but it is always there.",
+  "In the midst of movement and chaos, keep stillness inside of you.",
+  "We can't control the sea, but we can learn how to surf the waves.",
+  "Feelings come and go like clouds in a windy sky. Conscious breathing is my anchor.",
+  "To understand the immeasurable, the mind must be extraordinarily quiet, still.",
+];
+
+const colors = [
+  "#e81416",
+  "#ffa500",
+  "#faeb36",
+  "#79c314",
+  "#487de7",
+  "#4b369d",
+  "#70369d",
+];
+
+let wrapperDiv = document.getElementById("wrapper");
+let quoteText = document.getElementById("quote-text");
+let quoteButton = document.getElementById("quote-button");
+
+quoteButton.addEventListener("click", function () {
+  let randomInsex = Math.floor(Math.random() * quoteList.length);
+  quoteText.innerText = quoteList[randomInsex];
+  wrapperDiv.style.backgroundColor = colors[randomInsex];
+});
