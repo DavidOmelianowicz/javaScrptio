9ccbc244f79c83158cfa4e2671797a1a95e61a73
David Omelianowicz <david_omeli@hotmail.com>
2025-10-23 17:00:12 +0100

    img and css on meme master

--- codexlight/codex.js
+++ codexlight/codex.js
@@ -1,8 +1,8 @@
 const memeArray = [
-  "https://i.imgur.com/bSi4xLb.png",
-  "https://i.imgur.com/6y0G7N0.png",
-  "https://i.imgur.com/LXnRao1.png",
-  "https://i.imgur.com/Qqoxh1N.png",
+  "https://th.bing.com/th/id/OIP.HOnVgdsfXq_cSJVGgiJr8wHaE7?w=259&h=180&c=7&r=0&o=7&cb=12&dpr=2&pid=1.7&rm=3",
+  "https://th.bing.com/th/id/OIP.KdEuF9h8XSxRuDJScKsqhQHaE8?w=232&h=180&c=7&r=0&o=7&cb=12&dpr=2&pid=1.7&rm=3",
+  "https://th.bing.com/th/id/OIP.GWPg2soOegU8SZ2CBoLNYgHaE8?w=294&h=197&c=7&r=0&o=7&cb=12&dpr=2&pid=1.7&rm=3",
+  "https://th.bing.com/th/id/OIP.uGnSHQobLta2-a2quS7KMgHaE8?w=286&h=191&c=7&r=0&o=7&cb=12&dpr=2&pid=1.7&rm=3",
 ];
 
 const captionArray = [
@@ -15,3 +15,11 @@ const captionArray = [
 const randomMeme = document.getElementById("random-meme");
 const randomCaption = document.getElementById("random-caption");
 const generatorButton = document.getElementById("generator-button");
+
+generatorButton.addEventListener("click", () => {
+  const randomMemeIndex = Math.floor(Math.random() * memeArray.length);
+  const randomCaptionIndex = Math.floor(Math.random() * captionArray.length);
+
+  randomMeme.src = memeArray[randomMemeIndex];
+  randomCaption.textContent = captionArray[randomCaptionIndex];
+});
