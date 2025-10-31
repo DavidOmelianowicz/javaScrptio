bfb5383d6ade53d30eb37883ee8a513e7cb6e40f
David Omelianowicz <david_omeli@hotmail.com>
2025-10-31 18:08:15 +0000

    cleaning and leaving coments

--- flappy_bird/app.js
+++ flappy_bird/app.js
@@ -49,10 +49,6 @@ function create() {
     repeat: 1,
     setXY: { x: 350, y: 400, stepX: 300 },
   });
-
-  this.physics.add.collider(bird, topColumns);
-  this.physics.add.collider(bird, bottomColumns);
-
   const road = roads.create(400, 568, "road").setScale(2).refreshBody();
 
   // create the bird sprite
@@ -61,6 +57,8 @@ function create() {
   bird.setCollideWorldBounds(true);
 
   // collisions
+  this.physics.add.collider(bird, topColumns);
+  this.physics.add.collider(bird, bottomColumns);
   this.physics.add.collider(bird, road);
   this.physics.add.overlap(bird, road, () => (hasLanded = true), null, this);
   this.physics.add.collider(bird, road);
@@ -90,17 +88,20 @@ function update() {
   if (cursors.space.isDown && !isGameStarted) {
     isGameStarted = true;
   }
+  // bird starting position
   if (!isGameStarted) {
     bird.setVelocityY(-160);
   }
-
+  // bird upward movement
   if (cursors.up.isDown && !hasLanded && !hasBumped) {
     bird.setVelocityY(-160);
   }
+  // bird forward movement
   if (!hasLanded || !hasBumped) {
-    bird.body.setVelocityX = 50;
+    bird.body.setVelocityX(50);
   }
+  // stop bird movement on landing or bumping
   if (hasLanded || hasBumped || !isGameStarted) {
-    bird.body.setVelocityX = 0;
+    bird.body.setVelocityX(0);
   }
 }
